527942bfe95720c73fe349e9407e7e154a3c20b2
Felipe Ferreira <felipado2x0@gmail.com>
2024-08-23 22:21:56 -0300

    docs: finished community page

--- src/components/Communities/CommunityChoice/index.js
+++ src/components/Communities/CommunityChoice/index.js
@@ -1,62 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
-import community1 from "../../../assets/Images/Communities/community1.png";
-import community2 from "../../../assets/Images/Communities/community2.png";
-import community3 from "../../../assets/Images/Communities/community3.png";
-import community4 from "../../../assets/Images/Communities/community4.png";
 
 export default function ComunidadesEscolha({ comunidades }) {
-    const imgComunidades = [
-        community1,
-        community2,
-        community3,
-        community4
-    ];
 
-    const [index, setIndex] = useState(0);
+    // useState começa no primeiro item
+    const [indexAtual, setIndexAtual] = useState(comunidades.length);
 
     // Quantas imagens aparecerão no slider
     const imgsPorSlider = 4;
 
-    // Função para passar o slider
-    const mudarImagem = (direcao) => {
-        setIndex((prevIndex) => {
-            let newIndex = prevIndex + direcao * imgsPorSlider;
-            
-            if (newIndex < 0) {
-                newIndex = imgComunidades.length - imgsPorSlider;     
-            } else if (newIndex >= imgComunidades.length) {
-                newIndex = 0;
-            }
+    // Duplicar as imagens
+    const sliderRef = useRef();
 
-            return newIndex;
-        });
+    // Array de duplicação de imagens
+    const totalComunidades = [...comunidades, ...comunidades, ...comunidades];
+
+    // Função para passar o slide
+    const proxSlide = () => {
+        setIndexAtual((antSlide) =>
+            antSlide === comunidades.length - 1 ? 0 : antSlide + 1
+        );
+    };
+
+    // Função para voltar o slide
+    const antSlide = () => {
+        setIndexAtual((antSlide) =>
+            antSlide === 0 ? comunidades.length - 1 : antSlide - 1
+        );
+    };
+
+    const imgsMostradas = [];
+    for (let i = 0; i < imgsPorSlider; i++) {
+        imgsMostradas.push(comunidades[(indexAtual + 1) % comunidades.length]);
     }
 
-    // Função de quantas imagens vão aparecer no slider
-    const imgsMostradas = () => {
-        let img = [];
-
-        for (let i = 0; i < imgsPorSlider; i++) {
-            img.push(imgComunidades[(index + i) % imgComunidades.length]);
-            
+    // useEffect para quando o index atingir seu limite,
+    // volta para o ponto correto da array
+    useEffect(() => {
+        if (indexAtual === totalComunidades.length - imgsPorSlider) {
+            setTimeout(() => {
+                sliderRef.current.style.transition = 'none';
+                setIndexAtual(comunidades.length);
+                sliderRef.current.style.transition = 'transform 0.5s ease';
+            }, 500);
         }
 
-        return img;
-    }
+        if (indexAtual === 0) {
+            setTimeout(() => {
+                sliderRef.current.style.transition = 'none';
+                setIndexAtual(comunidades.length);
+                sliderRef.current.style.transition = 'transform 0.5s ease';
+            }, 500);
+        }
+    }, [indexAtual, comunidades.length, totalComunidades.length]);
 
     return (
         <div className="comunidade-escolha">
             <h1 className="comunidade-escolha-titulo">Comunidades</h1>
             <div className="comunidade-escolha-slider-area">
-                <RiArrowLeftSLine className="comunidade-escolha-antes" onClick={() => mudarImagem(-1)} />
-                <div className="comunidade-escolha-slider">
-                    {imgsMostradas().map((img, index) => (
-                       <img className="comunidade-escolha-img" src={img} key={index} alt={`Comunidade ${index}`} />
+                <RiArrowLeftSLine className="comunidade-escolha-antes" onClick={antSlide} />
+                <div 
+                    className="comunidade-escolha-slider"
+                    ref={sliderRef} 
+                    style={{ transform:`translateX(-${indexAtual * (100 / imgsPorSlider)}%)` }}
+                >
+                    {totalComunidades.map((img, index) => (
+                       <img 
+                            className="comunidade-escolha-img" 
+                            src={img} 
+                            key={index} 
+                            alt={`Comunidade ${indexAtual + index}`} 
+                        />
                     ))}
                 </div>
-                <RiArrowRightSLine className="comunidade-escolha-depois" onClick={() => mudarImagem(1)} />
+                <RiArrowRightSLine className="comunidade-escolha-depois" onClick={proxSlide} />
             </div>
         </div>
     );
--- src/components/Communities/index.js
+++ src/components/Communities/index.js
@@ -5,8 +5,23 @@ import Pesquisa from "../Search";
 import ComunidadesPopular from "./PopularCommunities";
 import ComunidadesEscolha from "./CommunityChoice";
 import Footer from "../Footer";
+import community1 from "../../assets/Images/Communities/community1.png";
+import community2 from "../../assets/Images/Communities/community2.png";
+import community3 from "../../assets/Images/Communities/community3.png";
+import community4 from "../../assets/Images/Communities/community4.png";
+import community5 from "../../assets/Images/Communities/community5.png";
+import community6 from "../../assets/Images/Communities/community6.png";
 
 export default function ComunidadesArea() {
+    const imgComunidades = [
+        community1,
+        community2,
+        community3,
+        community4,
+        community5,
+        community6
+    ];
+
     return (
         <div className="comunidade-pagina">
             <Header />
@@ -14,7 +29,7 @@ export default function ComunidadesArea() {
                 <Pesquisa className="comunidade-search" placeholder={"Pesquisar comunidades"} />
                 <ComunidadesPopular />
                 <hr className="comunidade-divisor" />
-                <ComunidadesEscolha />
+                <ComunidadesEscolha comunidades={imgComunidades} />
                 <Footer />
             </main>
         </div>
