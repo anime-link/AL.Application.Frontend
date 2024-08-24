0032af6850fa6154e8bc67c0ff6275992cdfe95f
unknown <williampatriciodosantos28@gmail.com>
2024-08-23 22:30:29 -0300

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,7 +4,7 @@ import LoginArea from "../../components/Login";
 
 export default function Login() {
   return (
-    <div className="login-area" >
+    <div className="login-page" >
       <LoginArea/>
     </div>
   );
