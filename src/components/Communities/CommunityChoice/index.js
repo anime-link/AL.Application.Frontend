9365afd9f7e81a02a9773a4ed9acc3544b167c43
Felipe Ferreira <felipado2x0@gmail.com>
2024-08-26 13:35:57 -0300

    docs: changed logic of community choice slider

--- src/components/Communities/CommunityChoice/index.js
+++ src/components/Communities/CommunityChoice/index.js
@@ -5,55 +5,31 @@ import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 export default function ComunidadesEscolha({ comunidades }) {
 
     // useState começa no primeiro item
-    const [indexAtual, setIndexAtual] = useState(comunidades.length);
+    const [indexAtual, setIndexAtual] = useState(0);
 
     // Quantas imagens aparecerão no slider
     const imgsPorSlider = 4;
 
-    // Duplicar as imagens
-    const sliderRef = useRef();
-
-    // Array de duplicação de imagens
-    const totalComunidades = [...comunidades, ...comunidades, ...comunidades];
-
     // Função para passar o slide
     const proxSlide = () => {
         setIndexAtual((antSlide) =>
-            antSlide === comunidades.length - 1 ? 0 : antSlide + 1
+            antSlide < comunidades.length - imgsPorSlider ? antSlide + 1 : antSlide
         );
     };
 
     // Função para voltar o slide
     const antSlide = () => {
         setIndexAtual((antSlide) =>
-            antSlide === 0 ? comunidades.length - 1 : antSlide - 1
+            antSlide > 0 ? antSlide - 1 : antSlide
         );
     };
 
     const imgsMostradas = [];
     for (let i = 0; i < imgsPorSlider; i++) {
-        imgsMostradas.push(comunidades[(indexAtual + 1) % comunidades.length]);
+        if (indexAtual + i < comunidades.length) {
+            imgsMostradas.push(comunidades[indexAtual + i]);   
+        }
     }
-
-    // useEffect para quando o index atingir seu limite,
-    // volta para o ponto correto da array
-    useEffect(() => {
-        if (indexAtual === totalComunidades.length - imgsPorSlider) {
-            setTimeout(() => {
-                sliderRef.current.style.transition = 'none';
-                setIndexAtual(comunidades.length);
-                sliderRef.current.style.transition = 'transform 0.5s ease';
-            }, 500);
-        }
-
-        if (indexAtual === 0) {
-            setTimeout(() => {
-                sliderRef.current.style.transition = 'none';
-                setIndexAtual(comunidades.length);
-                sliderRef.current.style.transition = 'transform 0.5s ease';
-            }, 500);
-        }
-    }, [indexAtual, comunidades.length, totalComunidades.length]);
 
     return (
         <div className="comunidade-escolha">
@@ -62,10 +38,9 @@ export default function ComunidadesEscolha({ comunidades }) {
                 <RiArrowLeftSLine className="comunidade-escolha-antes" onClick={antSlide} />
                 <div 
                     className="comunidade-escolha-slider"
-                    ref={sliderRef} 
                     style={{ transform:`translateX(-${indexAtual * (100 / imgsPorSlider)}%)` }}
                 >
-                    {totalComunidades.map((img, index) => (
+                    {comunidades.map((img, index) => (
                        <img 
                             className="comunidade-escolha-img" 
                             src={img} 
