0ee8298138cc33b7882811bff0b0fc8397c6ca52
William Patricio <williampatriciodosantos@gmail.com>
2024-09-04 01:55:43 -0300

    animes

--- src/components/AnimesPage/AnimesRanking/index.js
+++ src/components/AnimesPage/AnimesRanking/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import "./styles.css";
+import { RiAwardFill } from "react-icons/ri";
 
-
-export default function AnimesRanking({anime, number}){
-    return(
-        <div className= "ranking-area">
-           <h2 className= "ranking-titulo">
-           </h2>
-           <div className= "ranking-box">
-            {number}
-            {anime}
-           </div>
+export default function AnimesRanking({ rankingList }) {
+    return (
+        <div className="ranking-area">
+            <div className="ranking-box">
+                <h2 className="ranking-titulo">Top 5</h2>
+                {rankingList.map((item, index) => (
+                    <div key={index} className="ranking-cont">
+                        <span className="ranking-number">{item.number}</span>
+                        <span className="ranking-anime">{item.anime}</span>
+                        <RiAwardFill />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
--- src/components/AnimesPage/index.js
+++ src/components/AnimesPage/index.js
@@ -5,17 +5,45 @@ import Pesquisa from "../Search";
 import AnimesesLancamentos from './AnimesLaunch';
 import AnimesRanking from './AnimesRanking';
 
+export default function AnimesArea() {
+    // Lista de animes e seus números de ranking
+    const ranking1 = [
+        { anime: 'Naruto', number: '1' },
+        { anime: 'Naruto', number: '2' },
+        { anime: 'Naruto', number: '3' },
+        { anime: 'Naruto', number: '4' },
+        { anime: 'Naruto', number: '5' },
+        // Adicione mais itens conforme necessário
+    ];
 
-export default function AnimesArea(){
+    const ranking2 = [
+        { anime: 'Naruto', number: '1' },
+        { anime: 'Noragami', number: '2' },
+        { anime: 'Naruto', number: '3' },
+        { anime: 'Naruto', number: '4' },
+        { anime: 'Naruto', number: '5' },
+        // Adicione mais itens conforme necessário
+    ];
 
 
-    return(
+    return (
         <div className="animes-area">
-            <Header/>
+            <Header />
             <main className="animes-body">
-                <Pesquisa className="animes-search" placeholder={"Pesquisar Animes"} />
-                <AnimesesLancamentos/>
-                <AnimesRanking anime='teste' number='umnumeroqualquer'/>
+                <Pesquisa className="animes-search" placeholder="Pesquisar Animes" />
+                <AnimesesLancamentos />
+                <div className= "ranking-container">
+
+                <div className= "list1">
+                <AnimesRanking rankingList={ranking1} />
+                </div>
+
+                <div className= "list2">
+                <AnimesRanking rankingList={ranking2} />
+                </div>
+
+                </div>
+                
                 {/* <Footer /> */}
             </main>
         </div>
