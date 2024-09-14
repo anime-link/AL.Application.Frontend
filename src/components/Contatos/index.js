fd1679765adb333b986b8155195deb10429d355e
unknown <williampatriciodosantos28@gmail.com>
2024-09-13 22:18:19 -0300

    docs: put header

--- src/components/Contatos/index.js
+++ src/components/Contatos/index.js
@@ -3,7 +3,14 @@ import "./styles.css";
 import Header from '../Header';
 import ContatosBemVindo from './ConatosPresentation';
 import ContatosAdms from './ContatosAdm';
-import bluelock from '../../assets/Images/Contatos/bluelock.png';
+
+import perfilCarlos from '../../assets/Images/Contatos/perfilCarlos.png';
+import perfilFelipe from '../../assets/Images/Contatos/perfilFelipe.png';
+import perfilJoao from '../../assets/Images/Contatos/perfilJoao.png';
+import perfilNathan from '../../assets/Images/Contatos/perfilNathan.png';
+import perfilPablo from '../../assets/Images/Contatos/perfilPablo.png';
+import perfilWilliam from '../../assets/Images/Contatos/perfilWilliam.png';
+import perfilWilson from '../../assets/Images/Contatos/perfiWilson.png';
 
 import backgroundCarlos from '../../assets/Images/Contatos/backgoundCarlos.png';
 import backgroundFelipe from '../../assets/Images/Contatos/backgroundFelipe.png';
@@ -16,7 +23,7 @@ import backgroundWilson from '../../assets/Images/Contatos/backgroundWilson.png'
 
 
 
-import { RiTrophyFill } from "react-icons/ri";
+import { RiGithubFill } from "react-icons/ri";
 
 
 
@@ -26,16 +33,14 @@ export default function ContatosAdm() {
             <Header />
             <main className="contatos-body">
                 <ContatosBemVindo />
-
                 <div className= "spaceAdm"> 
-                <ContatosAdms  imgAnime={backgroundCarlos} ImgAdm={bluelock} name={"Carlos André"} Icons={<RiTrophyFill/>} />
-                <ContatosAdms imgAnime={backgroundFelipe} ImgAdm={bluelock} name={"Felipe Ferreira"} Icons={<RiTrophyFill  className="icon-blue"/>} />
-                <ContatosAdms imgAnime={backgroundJoao} ImgAdm={bluelock} name={"João Vitor"} Icons={<RiTrophyFill  className="icon-blue" />} />
-                <ContatosAdms imgAnime={backgroundNathan} ImgAdm={bluelock} name={"Nathan Rodrigues" } Icons={<RiTrophyFill  className="icon-blue"/>} />
-                <ContatosAdms imgAnime={backgroundPablo} ImgAdm={bluelock} name={"Pablo Candido" } Icons={<RiTrophyFill  className="icon-blue"/>} />
-                <ContatosAdms imgAnime={backgroundWilliam} ImgAdm={bluelock} name={"William Patricio" } Icons={<RiTrophyFill  className="icon-blue"/>} />
-                <ContatosAdms imgAnime={backgroundWilson} ImgAdm={bluelock} name={"Wilson B" } Icons={<RiTrophyFill  className="icon-blue"/>} />
-                
+                <ContatosAdms  imgAnime={backgroundCarlos} ImgAdm={perfilCarlos} name={"Carlos André"} Icons={<RiGithubFill className="icon-blue"/>} />
+                <ContatosAdms imgAnime={backgroundFelipe} ImgAdm={perfilFelipe} name={"Felipe Ferreira"} Icons={<RiGithubFill  className="icon-blue"/>} />
+                <ContatosAdms imgAnime={backgroundJoao} ImgAdm={perfilJoao} name={"João Vitor"} Icons={<RiGithubFill  className="icon-blue" />} />
+                <ContatosAdms imgAnime={backgroundNathan} ImgAdm={perfilNathan} name={"Nathan Rodrigues" } Icons={<RiGithubFill  className="icon-blue"/>} />
+                <ContatosAdms imgAnime={backgroundPablo} ImgAdm={perfilPablo} name={"Pablo Candido" } Icons={<RiGithubFill  className="icon-blue"/>} />
+                <ContatosAdms imgAnime={backgroundWilliam} ImgAdm={perfilWilliam} name={"William Patricio" } Icons={<RiGithubFill  className="icon-blue"/>} />
+                <ContatosAdms imgAnime={backgroundWilson} ImgAdm={perfilWilson} name={"Wilson Benacchio" } Icons={<RiGithubFill  className="icon-blue"/>} />
                 </div>
             </main>
         </div>
---
+++ src/components/Sinopse/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css';
+import Header from '../Header';
+
+export default function SinopseArea() {
+  return (
+    <div className="sinopse-area">
+      <Header/>
+    </div>
+   
+  );
+
+}
