bfb9299a0a5ab321a56e6619f8a8fdd5dcc0681b
unknown <williampatriciodosantos28@gmail.com>
2024-09-23 21:09:43 -0300

    finish sinopse/launch

--- src/components/AnimesPage/AnimesRanking/index.js
+++ src/components/AnimesPage/AnimesRanking/index.js
@@ -5,7 +5,7 @@ import { RiTrophyFill } from "react-icons/ri";
 export default function AnimesRanking({ colocacao, rankingNome }) {
     return (
         <div className="ranking-area">
-            <p className="ranking-colocação">{colocacao}</p>
+            <p className="ranking-colocacao">{colocacao}</p>
             <p className="ranking-nome">{rankingNome}</p>
             <RiTrophyFill className="trofeu"/>
         </div>
--- src/components/AnimesPage/index.js
+++ src/components/AnimesPage/index.js
@@ -15,15 +15,6 @@ import esporte from '../../assets/Images/AnimesCard/esporte.png';
 import Footer from "../Footer";
 
 export default function AnimesArea() {
-    // Lista de animes e seus números de ranking
-    // const ranking1 = [
-    //     { anime: 'Naruto', number: '1' },
-    //     { anime: 'Noragami', number: '2' },
-    //     { anime: 'Blue Lock', number: '3' },
-    //     { anime: 'Horimyia', number: '4' },
-    //     { anime: 'Boku no Hero', number: '5' },
-    // ];
-
     
     return (
         <div className="animes-area">
@@ -34,11 +25,11 @@ export default function AnimesArea() {
                 <div className="ranking-container">
 
                     <p className="titulo-card">Top 5</p>
-                    <AnimesRanking colocacao={"1"}  rankingNome={"Blue Lock"} />
-                    <AnimesRanking colocacao={"1"}  rankingNome={"Blue Lock"} />
-                    <AnimesRanking colocacao={"1"}  rankingNome={"Blue Lock"} />
-                    <AnimesRanking colocacao={"1"}  rankingNome={"Blue Lock"} />
-                    <AnimesRanking colocacao={"1"}  rankingNome={"Blue Lock"} />
+                    <AnimesRanking colocacao={"1"}  rankingNome={"Naruto"} />
+                    <AnimesRanking colocacao={"2"}  rankingNome={"One Piece"} />
+                    <AnimesRanking colocacao={"3"}  rankingNome={"Noragami"} />
+                    <AnimesRanking colocacao={"4"}  rankingNome={"Classroom of The Elite"} />
+                    <AnimesRanking colocacao={"5"}  rankingNome={"Goblins Layer"} />
                     
                 </div>
                 <hr className="ranking-divisao" />
