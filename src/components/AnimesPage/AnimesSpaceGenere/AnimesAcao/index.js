bb259a752a8ff2c8ff6ff6ab8177dbc78f3d8222
unknown <williampatriciodosantos28@gmail.com>
2024-10-28 22:12:46 -0300

    configGender

--- src/components/AnimesPage/AnimesSpaceGenere/AnimesAcao/index.js
+++ src/components/AnimesPage/AnimesSpaceGenere/AnimesAcao/index.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom'; 
-import { RiArrowLeftCircleFill } from "react-icons/ri";
-
+import { RiArrowLeftCircleFill, RiArrowRightCircleFill } from "react-icons/ri";
 import CardAcao from "./CardAcao";
 import Footer from "../../../Footer";
 import Header from '../../../Header';
 import { getJikanGenres } from '../../../../services/AnimeAPI/AnimeApi';
 
-
-
 export default function AnimesAcao() {
     const [animes, setAnimes] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const itemsPerPage = 5;
 
     useEffect(() => {
         const fetchAnimes = async () => {
             try {
-                const animeGenre = await getJikanGenres(4);
+                const animeGenre = await getJikanGenres(1);
                 setAnimes(animeGenre);
             } catch (error) {
                 console.error('Erro ao buscar os animes de tal gênero: ', error);
@@ -27,25 +26,36 @@ export default function AnimesAcao() {
         }
 
         fetchAnimes();
-
     }, []);
 
     if (loading) return <div>Carregando...</div>;
 
+    const handleNext = () => {
+        if (currentIndex + itemsPerPage < animes.length) {
+            setCurrentIndex(currentIndex + itemsPerPage);
+        }
+    };
+
+    const handlePrev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - itemsPerPage);
+        }
+    };
+
     return (
         <div className="acao-area">
-            <Header/>
-            <main className= "acao-body">
-                <diV className = "acao-back">
-                    <diV className="acao-icon-voltar">
+            <Header />
+            <main className="acao-body">
+                <div className="acao-back">
+                    <div className="acao-icon-voltar">
                         <Link className="icon-return" to={"/animes"}>
-                            <RiArrowLeftCircleFill  alt= "Fechar"/>
+                            <RiArrowLeftCircleFill className="button-return" alt="Fechar" />
                         </Link>
-                    </diV>
+                    </div>
                     <p className="acao-tittle">Ação</p>
-                </diV>
+                </div>
                 <div>
-                    {animes.map(anime => (
+                    {animes.slice(currentIndex, currentIndex + itemsPerPage).map(anime => (
                         <CardAcao 
                             key={anime.id}
                             cardImgAcao={anime.image}
@@ -54,9 +64,17 @@ export default function AnimesAcao() {
                         />
                     ))}
                 </div>
-                
+                <div className="translate">
+                    <Link className="teste-1" onClick={handlePrev}>
+                        <RiArrowLeftCircleFill className="button-translate" />
+                    </Link>
+                    
+                    <Link className="teste-2" onClick={handleNext}>
+                        <RiArrowRightCircleFill className="button-translate" />
+                    </Link>
+                </div>
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
