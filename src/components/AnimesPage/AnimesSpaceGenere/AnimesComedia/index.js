cb6f223254e1deac7bc5982c00e6d1527b436814
felipeFerreiraffl <felipado2x0@gmail.com>
2024-11-01 19:11:08 -0300

    docs: transformed chat page in a responsive page

--- src/components/AnimesPage/AnimesSpaceGenere/AnimesComedia/index.js
+++ src/components/AnimesPage/AnimesSpaceGenere/AnimesComedia/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import { RiArrowLeftCircleFill } from "react-icons/ri";
 
 import CardComedia from "./CardComedia";
@@ -12,30 +12,29 @@ import Header from '../../../Header';
 export default function AnimesComedia() {
     return (
         <div className="comedia-area">
-            <Header/>
-            <main className= "comedia-body">
-                <diV className = "comedia-back">
+            <Header />
+            <main className="comedia-body">
+                <div className="comedia-back">
                     <diV className="comedia-icon-voltar">
                         <Link className="icon-return" to={"/animes"}>
-                            <RiArrowLeftCircleFill  alt= "Fechar"/>
+                            <RiArrowLeftCircleFill alt="Fechar" />
                         </Link>
                     </diV>
                     <p className="comedia-tittle">Comedia</p>
-                </diV>
-                <div></div>
+                </div>
 
                 <div>
-                <CardComedia/>
-                <CardComedia/>
-                <CardComedia/>
-                <CardComedia/>
-                <CardComedia/>
-                <CardComedia/>
-                <CardComedia/>
+                    <CardComedia />
+                    <CardComedia />
+                    <CardComedia />
+                    <CardComedia />
+                    <CardComedia />
+                    <CardComedia />
+                    <CardComedia />
                 </div>
-                
+
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
