b360e602d63184648c497d7801290563a8c9d6c0
felipeFerreiraffl <felipado2x0@gmail.com>
2024-11-01 21:36:06 -0300

    feat: added forgot and change password logic and validation

--- src/components/ChangePassword/ChangeArea/index.jsx
+++ src/components/ChangePassword/ChangeArea/index.jsx
@@ -1,19 +1,71 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
+import { useState } from 'react';
 
-export default function MudarSenhaArea({ id, onClick }) {
+export default function MudarSenhaArea() {
+    const [novaSenha, setNovaSenha] = useState('');
+    const [confirmarSenha, setConfirmarSenha] = useState('');
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (novaSenha !== confirmarSenha) {
+            alert("Senhas não coincidem");
+        }
+
+        try {
+            const response = await fetch(`http://localhost:8080/usuario/redefinir-senha?token=${token}&novaSenha=${novaSenha}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            if (response.ok) {
+                alert('Senha redefinida com sucesso.');
+                navigate('/login');
+
+            } else if (response.status === 401) {
+                alert('Token inválido ou expirado.');
+            } else if (response.status === 404) {
+                alert('Usuário não encontrado.');
+            } else {
+                alert('Erro ao redefinir a senha. Tente novamente.');
+            }
+
+        } catch (error) {
+            console.error("Erro ao redefinir a senha");
+            alert("Erro ao redefinir a senha");
+        }
+    }
+
     return (
         <div className='mudar-area'>
             <h2 className='mudar-titulo'>Crie uma nova senha</h2>
-            <form className='mudar-form-area'>
+            <form className='mudar-form-area' onSubmit={handleSubmit}>
                 <div className='mudar-form'>
-                    <input className='mudar-input' id={id} placeholder='Criar nova senha' />
-                    <input className='mudar-input' id={id} placeholder='Confirmar senha' />
-                    <input className='mudar-input' id={id} placeholder='Digitar o token' />
+                    <input
+                        className='mudar-input'
+                        placeholder='Criar nova senha'
+                        type='password'
+                        value={novaSenha}
+                        onChange={(e) => setNovaSenha(e.target.value)}
+                    />
+                    <input
+                        className='mudar-input'
+                        placeholder='Confirmar senha'
+                        type='password'
+                        value={confirmarSenha}
+                        onChange={(e) => setConfirmarSenha(e.target.value)}
+                    />
+                    <input
+                        className='mudar-input'
+                        placeholder='Digitar o token'
+                        value={token}
+                        onChange={(e) => setToken(e.target.value)}
+                    />
                 </div>
-                <Link to={'/login'}>
-                    <button className='mudar-botao' type='submit' onClick={onClick}>Confirmar</button>
-                </Link>
+                <button className='mudar-botao' type='submit'>Confirmar</button>
             </form>
         </div>
     );
