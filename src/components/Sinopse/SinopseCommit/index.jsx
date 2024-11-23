d3af7983d070b867cb941eca46da3461da21454c
unknown <williampatriciodosantos28@gmail.com>
2024-11-22 22:13:59 -0300

    configVote

--- src/components/Sinopse/SinopseCommit/index.jsx
+++ src/components/Sinopse/SinopseCommit/index.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 import imgVolte from "../../../assets/Images/Sinopse/imgVolte.png";
 
 import { RiThumbUpFill } from "react-icons/ri";
 import { RiThumbDownFill } from "react-icons/ri";
 
-
-
 export default function SinopseCommit() {
-  return (
-    <div style={{ backgroundImage: `url(${imgVolte})` }} className="sinopse-volte" >
+  // Inicializando os estados para os contadores de likes e dislikes
+  const [likes, setLikes] = useState(0);  // Começando com 1000 likes
+  const [dislikes, setDislikes] = useState(0);  // Começando com 1000 dislikes
 
-      <div className="content-volte">
+  // Função para incrementar o número de likes
+  const handleLike = () => {
+    setLikes(likes + 1);  // Incrementa 1 no contador de likes
+  };
+
+  // Função para incrementar o número de dislikes
+  const handleDislike = () => {
+    setDislikes(dislikes + 1);  // Incrementa 1 no contador de dislikes
+  };
+
+  return (
+    <div style={{ backgroundImage: `url(${imgVolte})` }} className="sinopse-vote">
+      <div className="content-vote">
         <p className="title-vote">AVALIE AQUI</p>
-        <div className="volte-like">
-          <button>
+        <div className="vote-like">
+          <button onClick={handleLike}>
             <RiThumbUpFill className="likeUp"/>
-            <p>1000k</p>
+            <p>{likes}</p>  {/* Exibe o número de likes */}
           </button>
-          <button>
+          <button onClick={handleDislike}>
             <RiThumbDownFill className="likeDown"/>
-            <p>1000k</p>
+            <p>{dislikes}</p>  {/* Exibe o número de dislikes */}
           </button>
         </div>
       </div>
-     
     </div>
   );
-
 }
