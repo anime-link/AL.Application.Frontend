8cbd4e7a236e38a9f5a327d479d2c2b60ad6f30d
unknown <williampatriciodosantos28@gmail.com>
2024-11-26 22:11:35 -0300

    configPerfil

--- src/components/UserPage/UserInput/index.jsx
+++ src/components/UserPage/UserInput/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './styles.css';
 
-export default function UsuarioInput({ label, placeholder }) {
+export default function UsuarioInput({ label, onChange, type, value }) {
     return (
         <div className="usuario-label-input">
             <label className="usuario-label">{label}</label>  
-            <input className="usuario-input" />
+            <input 
+                className="usuario-input" 
+                value={value}
+                onChange={onChange}
+                type={type}
+            />
         </div>
     );
 }
--- src/components/UserPage/index.jsx
+++ src/components/UserPage/index.jsx
@@ -1,40 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './styles.css';
 import Header from '../Header/index.jsx';
 import { RiPencilFill } from 'react-icons/ri';
+import { RiBrushLine } from "react-icons/ri";
 import UsuarioInput from "./UserInput/index.jsx";
 import defaultImg from '../../assets/Images/SignUp/profile-pic-choosen.jpeg';
+
+import { useUser } from '../../services/UserContext/index';
 import { useProfileImage } from "../../services/PicContext/index.js";
 
+
+import logoHxh from "../../assets/Images/User/logoHxh.png"
+
 export default function Usuario() {
-    const { profileImage } = useProfileImage();
+
+    const { user } = useUser();
+    const [userData, setUserData] = useState({
+        nome: "",
+        email: "",
+        senha: "",
+    });
+    const { profileImage, setProfileImage } = useProfileImage();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOnChangeImg = (newImg) => {
+        setProfileImage(newImg);
+        setIsModalOpen(false);
+
+    }
+
+    useEffect(() => {
+        setUserData({
+            nome: user?.name,
+            email: user?.email,
+            senha: user?.password
+        })
+
+        console.log("Dados do usuário: ", userData);
+    });
+
 
     return (
         <div className="usuario-area">
             <Header />
             <div className="usuario-infos">
-                <h1 className="usuario-titulo">Dados do usuário</h1>
                 <div className="usuario-infos-area">
                     <div className="usuario-foto-fav">
                         <img
                             className="usuario-foto"
                             src={profileImage || defaultImg}
-                            width={220}
-                            height={220}
+                            width={159}
+                            height={160}
                             alt="Perfil"
                         />
-                        <button className="usuario-botao-mudar">Mudar ícone</button>
+                        <button className="usuario-botao-mudar"> <RiBrushLine className="perfil-brush"/> </button>
+                        <input className="user-name" 
+                        type="text" 
+                        value={userData.nome}
+                        onChange={(e) => setUserData({ ...userData, nome: e.target.value })}
+                        />
                     </div>
                     <form className="usuario-form">
                         <div className="usuario-input-infos">
-                            <UsuarioInput label={"Nome"} />
-                            <UsuarioInput label={"Usuário"} />
-                            <UsuarioInput label={"E-mail"} />
-                            <UsuarioInput label={"Senha"} />
+                        
+                            <UsuarioInput
+                                label={"E-mail"}
+                                type={'text'}
+                                value={userData.email}
+                                onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+                            />
+                            <UsuarioInput
+                                label={"Senha"}
+                                type={'password'}
+                                value={userData.senha}
+                                onChange={(e) => setUserData({ ...userData, senha: e.target.value })}
+                            />
                         </div>
                         <div className="usuario-mudar-info">
                             <RiPencilFill color="var(--bright-sun)" fontSize={30} />
                             <button className="usuario-mudar-info-btn">Editar</button>
+                        </div>
+                        <div className="logo">
+                            <img 
+                            src={logoHxh} 
+                            alt="logo-perfil" />
                         </div>
                     </form>
                 </div>
