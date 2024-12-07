22b5ee0b50128f0f59455818725dee984fa65a3c
felipeFerreiraffl <felipado2x0@gmail.com>
2024-12-06 21:48:48 -0300

    docs: updated auth code to stay on the page

--- src/services/Auth/index.jsx
+++ src/services/Auth/index.jsx
@@ -3,16 +3,25 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false); // Controla o login
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    const storedAuth = localStorage.getItem("isAuthenticated");
+    return storedAuth === "true";
+  }); // Controla o login
 
-    const login = () => setIsAuthenticated(true); // Simula o login
-    const logout = () => setIsAuthenticated(false); // Simula o logout
-    
-    return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
+  const login = () => {
+    setIsAuthenticated(true);
+    localStorage.setItem("isAuthenticated", "true");
+  }; // Simula o login
+  const logout = () => {
+    setIsAuthenticated(false);
+    localStorage.setItem("isAuthenticated", "false");
+  }; // Simula o logout
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const useAuth = () => useContext(AuthContext);
