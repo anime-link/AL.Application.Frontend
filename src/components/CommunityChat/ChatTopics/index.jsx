b899eee9de8912904f2b63fe56fa387971224ffd
felipeFerreiraffl <felipado2x0@gmail.com>
2024-12-07 09:27:50 -0300

    docs: changed the image according the community image

--- src/components/CommunityChat/ChatTopics/index.jsx
+++ src/components/CommunityChat/ChatTopics/index.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import "./styles.css";
-import communityImg from '../../../assets/Images/Communities/community1.png';
+import community1 from '../../../assets/Images/Communities/community1.png';
+import community2 from '../../../assets/Images/Communities/community2.png';
+import community3 from '../../../assets/Images/Communities/community3.png';
+import community4 from '../../../assets/Images/Communities/community4.png';
+import community5 from '../../../assets/Images/Communities/community5.png';
+import community6 from '../../../assets/Images/Communities/community6.png';
 
-export default function ChatTopicos({ setCanalAtivo }) {
+export default function ChatTopicos({ setCanalAtivo, chatId }) {
     const [activeChannel, setActiveChannel] = useState("");
+
+    const communitiesImg = {
+        1: community1,
+        2: community2,
+        3: community3,
+        4: community4,
+        5: community5,
+        6: community6,
+    }
 
     const handleChannelClick = (canal) => {
         setActiveChannel(canal);
@@ -25,7 +39,7 @@ export default function ChatTopicos({ setCanalAtivo }) {
                 ))}
             </div>
             <div className="chat-topicos-img">
-                <img src={communityImg} alt="Comunidade" width={140} height={140} />
+                <img src={communitiesImg[chatId] || null} alt="Imagem da comunidade" width={140} height={140} />
                 <p>Comunidade</p>
             </div>
         </div>
--- src/components/CommunityChat/index.jsx
+++ src/components/CommunityChat/index.jsx
@@ -5,14 +5,30 @@ import ChatTopicos from "./ChatTopics";
 import ChatMensagem from "./ChatMessage";
 import { useParams } from "react-router-dom";
 
+import community1 from '../../assets/Images/Communities/community1.png';
+import community2 from '../../assets/Images/Communities/community2.png';
+import community3 from '../../assets/Images/Communities/community3.png';
+import community4 from '../../assets/Images/Communities/community4.png';
+import community5 from '../../assets/Images/Communities/community5.png';
+import community6 from '../../assets/Images/Communities/community6.png';
+
 export default function ChatComunidade() {
     const { chatId } = useParams(); // Extrai o ID do chat da URL
     const [canalAtivo, setCanalAtivo] = useState("canal");
 
+    const communitiesImg = {
+        1: community1,
+        2: community2,
+        3: community3,
+        4: community4,
+        5: community5,
+        6: community6,
+    };
+
     return (
-        <div className="comunidade-chat">
+        <div className="comunidade-chat" style={{ backgroundImage: `url(${communitiesImg[chatId]})` || null }}>
             <ChatHeader />
-            <ChatTopicos setCanalAtivo={setCanalAtivo} />
+            <ChatTopicos setCanalAtivo={setCanalAtivo} chatId={chatId} />
             <ChatMensagem canalAtivo={canalAtivo} chatId={chatId} /> {/* Passa o ID */}
         </div>
     );
